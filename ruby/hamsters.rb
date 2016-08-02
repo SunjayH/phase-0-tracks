0ee1381d66bd0ca46ddc68124c14332d3b5d39cf
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-02 16:42:59 -0700

    Print data

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -20,3 +20,16 @@ if age == ""
 else
 	age = age.to_i
 end
+
+puts "The hamster's name is #{name}."
+puts "The hamster's volume is #{volume}."
+puts "The hamster is #{color}."
+if adoption == true
+	puts "The hamster is a good candidate for adoption."
+else
+	puts "The hamster is not a good candidate for adoption."
+end
+if age != nil
+	puts "The hamster is #{age} years old."
+else
+end
