4f11da4cf151f1f08f47f91f9622685cc0bb5c64
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-11 12:04:46 -0700

    Did release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,7 +1,5 @@
 thisArray = ["cheese","sauce","pineapple", "pepperoni"]
-hashBrowns = {"firstElement" => "a",
-	"secondElement" => "b",
-	"thirdElement" => "c"}
+hashBrowns = {"firstElement" => "a", "secondElement" => "b", "thirdElement" => "c"}
 
 thisArray.each do |x|
 	puts "#{x} is a pizza topping! Delicious."
@@ -18,3 +16,24 @@ end
 hashBrowns.each do |x|
 	puts x
 end
+
+otherArray = [1,2,3,4,5,6,7,8,9]
+otherArray.delete_if {|item| item < 5}
+
+otherArray = [1,2,3,4,5,6,7,8,9]
+otherArray.keep_if {|item| item < 5}
+
+otherArray = [1,2,3,4,5,6,7,8,9]
+otherArray.select {|item| item.even?}
+
+otherArray.take_while {|item| item < 9}
+
+hashBrowns.delete_if {|key,value| value == "b"}
+
+hashBrowns = {"firstElement" => "a", "secondElement" => "b", "thirdElement" => "c"}
+hashBrowns.keep_if {|key,value| value == "b"}
+
+hashBrowns = {"firstElement" => "a", "secondElement" => "b", "thirdElement" => "c"}
+hashBrowns.select {|key,value| key == "firstElement"}
+
+hashBrowns.take_while {|key,value| key != "thirdElement"}
