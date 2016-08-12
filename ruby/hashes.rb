a241a7cce9a6c70d3dfed0d734dc9eaa08789cf2
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-12 10:14:12 -0700

    Deleted extra file, debugged. It works now yay!

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,6 +4,8 @@
 #Store in the hash as a pair
 #I'll deal with the last one later
 client_pref = {}
+#Used for bool conversion
+yes_values = ["y", "Y", "true"]
 
 puts "What's the client's name?"
 name_in = gets.chomp
@@ -23,7 +25,7 @@ client_pref[:decor] = decor_in
 
 puts "Does the client have pets?"
 pets_in = gets.chomp
-if pets_in.include? "Y"
+if yes_values.any? {|yes| pets_in.include? yes}
 		pets_in = true
 	else
 		pets_in = false
@@ -39,7 +41,7 @@ puts client_pref
 #Print new array
 
 puts "Would you like to change any of the entries?"
-if gets.chomp.include? "Y"
+if yes_values.any? {|yes| gets.chomp.include? yes}
 	puts "Which would you like to change?"
 	change_key = gets.chomp
 	puts "What is the new value?"
@@ -49,16 +51,14 @@ if gets.chomp.include? "Y"
 	elsif change_key == "children"
 		new_value = new_value.to_i
 	elsif change_key == "pets"
-		yes_values = ["y", "Y", "true"]
-		if yes_values.any {|yes| new_value.include? yes}
+		if yes_values.any? {|yes| new_value.include? yes}
 			new_value = true
 		else
 			new_value = false
 		end
 	else
 	end
-	change_key.to_sym
+	change_key = change_key.to_sym
 	client_pref[change_key] = new_value	
 end
 puts client_pref
-
