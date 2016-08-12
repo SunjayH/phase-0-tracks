838ff4a4527b3e938844478c68668a47f9b13bf5
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-12 13:03:26 -0700

    Finished the algorithm, tested with Felicia Torres and Zubat the Great. It worked

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,18 +2,24 @@
 def shift_values(string)
 	index = 0
 	#Lists for nexting
-	vowels_list = "aeiou"
-	consonants_list = "bcdfghjklmnpqrstvwxyz"
+	vowels_list = "AEIOUaeiou" #I thought about doing an upcase downcase thing but too much work
+	consonants_list = "BCDFGHIJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz"
 
 	#Consonants for sorting
 	vowels_array = vowels_list.split('')
 	consonants_array = consonants_list.split('')
 
+	#Edge case stuff
+	edge_array = ["z","Z","u","U"]
+	edge_replace = ["b", "B", "a", "A"]
+
 	#Choose if consonant or vowel, then shift in the list
 	while index < string.length		
-		#Don't forget to include downcase stuff!
-		if consonants_array.any? {|letter| string[index] == letter}
-			#Will also put in if for edge case z
+		#For edge case z, Z
+		if edge_array.any? {|letter| string[index] == letter}
+			position = edge_array.index(string[index])
+			string[index] = edge_replace[position]		
+		elsif consonants_array.any? {|letter| string[index] == letter}
 			position = consonants_list.index(string[index])
 			string[index] = consonants_list[position+1]
 		elsif vowels_array.any? {|letter| string[index] == letter}
@@ -49,12 +55,8 @@ def alias_maker(name)
 	name_array = name.split(' ')
 	name_array.reverse!
 	name = name_array.join(' ')
-	puts name
-
-
-
-
+	name = shift_values(name)
 end
 
-new_alias = alias_maker("Felicia Torres")
+new_alias = alias_maker("Zubat the Great")
 puts new_alias
--- ruby/shift_values.rb
+++ ruby/shift_values.rb
@@ -2,17 +2,24 @@
 def shift_values(string)
 	index = 0
 	#Lists for nexting
-	vowels_list = "aeiou"
-	consonants_list = "bcdfghjklmnpqrstvwxyz"
+	vowels_list = "AEIOUaeiou"
+	consonants_list = "BCDFGHIJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz"
 
 	#Consonants for sorting
 	vowels_array = vowels_list.split('')
 	consonants_array = consonants_list.split('')
 
+	#Edge case stuff
+	edge_array = ["z","Z","u","U"]
+	edge_replace = ["b", "B", "a", "A"]
+
 	#Choose if consonant or vowel, then shift in the list
 	while index < string.length		
-		if consonants_array.any? {|letter| string[index] == letter}
-			#Will also put in if for edge case z
+		#For edge case z, Z
+		if edge_array.any? {|letter| string[index] == letter}
+			position = edge_array.index(string[index])
+			string[index] = edge_replace[position]		
+		elsif consonants_array.any? {|letter| string[index] == letter}
 			position = consonants_list.index(string[index])
 			string[index] = consonants_list[position+1]
 		elsif vowels_array.any? {|letter| string[index] == letter}
@@ -27,4 +34,4 @@ def shift_values(string)
 	return string
 end
 
-puts shift_values("Torres")
+puts shift_values("Zubat")
