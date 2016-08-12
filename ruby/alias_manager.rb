503987bd316e25945c49f05e31734e74f0eb7a51
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-12 12:45:25 -0700

    Made consonant and vowel-shifting algorithm that works, notes about edge cases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,3 +1,33 @@
+#Cannibalized from last week
+def shift_values(string)
+	index = 0
+	#Lists for nexting
+	vowels_list = "aeiou"
+	consonants_list = "bcdfghjklmnpqrstvwxyz"
+
+	#Consonants for sorting
+	vowels_array = vowels_list.split('')
+	consonants_array = consonants_list.split('')
+
+	#Choose if consonant or vowel, then shift in the list
+	while index < string.length		
+		#Don't forget to include downcase stuff!
+		if consonants_array.any? {|letter| string[index] == letter}
+			#Will also put in if for edge case z
+			position = consonants_list.index(string[index])
+			string[index] = consonants_list[position+1]
+		elsif vowels_array.any? {|letter| string[index] == letter}
+			#Will also put in if for edge case u
+			position = vowels_list.index(string[index])
+			string[index] = vowels_list[position+1]
+		else
+			#Non-vowel characters will remain the same
+		end
+		index += 1
+	end
+	return string
+end
+
 #Split into words as array
 #Switch words in array
 #Return words to string
@@ -8,3 +38,23 @@
 #but do nothing to non-letter characters (probably spaces)
 #Return words to strings
 
+def alias_maker(name)
+	if name.class != String
+		puts "Error, name must be a string!"
+		return name
+	end
+	
+	#Reverse first and last name
+
+	name_array = name.split(' ')
+	name_array.reverse!
+	name = name_array.join(' ')
+	puts name
+
+
+
+
+end
+
+new_alias = alias_maker("Felicia Torres")
+puts new_alias
