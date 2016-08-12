6171631a89a08f19f97bb1e02320048bca4eaa97
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-12 12:01:35 -0700

    Pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+#Split into words as array
+#Switch words in array
+#Return words to string
+#Split into letters
+#Make string of vowels and consonants
+#Use .each to go through and change letters -
+#vowels to vowels, consonants to consonants,
+#but do nothing to non-letter characters (probably spaces)
+#Return words to strings
+
