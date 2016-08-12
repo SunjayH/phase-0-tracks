88029d996c1cb900b98b85ffcdcc36d23a70938d
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-12 10:57:12 -0700

    Made nested structure with pokemon

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+seattle_pokemon{
+	:drowzee => {
+		:name => "Drowzee",
+		:type => ["Psychic"],
+		:evolves_into => :hypno,
+		:favorite_places => "Inland, in urban areas with lots of bars"
+	}
+	:clefairy => {
+		:name => "Clefairy",
+		:type => ["Normal", "Fairy"],
+		:evolves_into => :clefable,
+		:favorite_places => "Inland, especially Capitol Hill"
+	}
+	:zubat => {
+		:name => "Zubat",
+		:type => ["Flying", "Poison"],
+		:evolves_into => :golbat,
+		:favorite_places => "Everywhere"
+	}
+	:magikarp => {
+		:name => "Magikarp",
+		:type => ["Water"]
+		:evolves_into => :gyarados,
+		:favorite_places => "Near lakes, such as Lake Union"
+	}
+	:golbat => {
+		:name => "Golbat",
+		:type => ["Flying", "Poison"],
+		:evolves_into => :crobat,
+		:favorite_places => "Everywhere but not as often"
+	}
+}
