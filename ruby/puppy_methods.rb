e5d93b267934ac87a12d11d81ff8a584765212ff
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-15 09:27:01 -0700

    Initialized kitten class with methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -24,11 +24,46 @@ class Puppy
   	puts "Woof"
   	#Puppy is a good boy
   end
+
+  def initialize
+  	puts "Initializing new puppy instance"
+  end
 end
 
-fido = Puppy.new
-fido.fetch("ball")
-fido.speak(4)
-fido.roll_over
-fido.dog_years(3)
-fido.whos_a_good_boy
+#fido = Puppy.new
+#fido.fetch("ball")
+#fido.speak(4)
+#fido.roll_over
+#fido.dog_years(3)
+#fido.whos_a_good_boy
+
+class Kitten
+	def initialize(color)
+		puts "Look at the adorable #{color} kitten!"
+	end
+
+	def fetch(toy)
+		puts "The #{toy} is still over there. Kittens don't fetch."
+	end
+
+	def num_lives(num)
+		puts "No, cats have 9 lives."
+	end
+
+	def feed
+		puts "The kitten wants more food still."
+	end
+
+end
+
+kitten_array = []
+
+50.times do |i|
+	kitten_array[i] = Kitten.new("black")
+end
+
+kitten_array.each do |kitten|
+	kitten.fetch("ball")
+	kitten.num_lives(1)
+	kitten.feed
+end
