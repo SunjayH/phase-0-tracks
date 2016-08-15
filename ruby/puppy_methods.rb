7d8d5be415229a3b2073a91d02380ceb0dcf46b4
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-15 09:13:48 -0700

    Made puppy methods file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,31 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(num)
+  	puts "Woof " * num
+  	num
+  end
 
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(age)
+  	age = age*7
+  	puts "Puppy is #{age} years old in dog years."
+  	age
+  end
+
+  def whos_a_good_boy
+  	puts "Woof"
+  	#Puppy is a good boy
+  end
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(4)
+fido.roll_over
+fido.dog_years(3)
+fido.whos_a_good_boy
