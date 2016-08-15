966133eea33ab7198f56af05a9300bcef42e1c2b
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-15 12:36:48 -0700

    Put characteristics of Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+class Puppy
+#Species: Canis familiaris
+#Characteristics:
+	#Friendly: yes
+	#Eye count: 2
+	#Leg count: 4
+	#Name: varies
+	#Color: varies
+#Behavior
+	#Bark
+	#Fetch
+	#Sniff things
+end
