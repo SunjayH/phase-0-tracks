9eb0c47126c135cd6828882f92d781ce36cf3068
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-16 16:18:35 -0700

    Added diverse Santa loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,7 +5,21 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
-	def initialize
+	def initialize(ethnicity, gender)
 		puts "Initializing Santa instance"
+		@ethnicity = ethnicity
+		@gender = gender
+		@age = 0
+		@reindeer_ranking = [
+			"Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
+			"Comet", "Cupid", "Donner", "Blitzen"
+		]
 	end
+end
+
+santas = []
+example_genders = ["male", "genderqueer", "female", "genderfluid", "agender"]
+example_ethnicities = ["black", "white", "hapa", "Khmer", "Jewish"]
+10.times do |i|
+	santas << Santa.new(example_genders.sample,example_ethnicities.sample)
 end
