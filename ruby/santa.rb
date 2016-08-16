809ddd21fa8d44d6f65d1584fb8147c3e3a9347f
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-16 16:37:21 -0700

    Added getters and setters, did test code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,4 @@
 class Santa
-	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
-	end
-	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie}!"
-	end
 	def initialize(ethnicity, gender)
 		puts "Initializing Santa instance"
 		@ethnicity = ethnicity
@@ -15,11 +9,59 @@ class Santa
 			"Comet", "Cupid", "Donner", "Blitzen"
 		]
 	end
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+	def celebrate_birthday
+		puts "Happy birthday Santa!"
+		@age += 1
+	end
+
+	def get_mad_at(name)
+		@reindeer_ranking.delete(name)
+		@reindeer_ranking << name
+	end
+
+	#Getter
+	def ethnicity
+		@ethnicity
+	end
+	def gender
+		@gender
+	end
+	def age
+		@age
+	end
+	def reindeer
+		@reindeer_ranking
+	end
+
+	#Setter
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
 end
 
 santas = []
-example_genders = ["male", "genderqueer", "female", "genderfluid", "agender"]
-example_ethnicities = ["black", "white", "hapa", "Khmer", "Jewish"]
+example_genders = ["male", "genderqueer", "female", "genderfluid"]
+example_ethnicities = ["black", "white", "hapa", "Hmong", "Jewish"]
 10.times do |i|
 	santas << Santa.new(example_genders.sample,example_ethnicities.sample)
 end
+
+interactive_santa = santas.sample
+puts interactive_santa.age
+puts interactive_santa.gender
+puts interactive_santa.ethnicity
+interactive_santa.celebrate_birthday
+interactive_santa.gender = "agender"
+puts interactive_santa.age
+puts interactive_santa.gender
+puts interactive_santa.reindeer
+interactive_santa.get_mad_at("Vixen")
+puts interactive_santa.reindeer
