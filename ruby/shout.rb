e2361bcffec83d89bd1fe096563b9c64472398a4
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-17 19:57:56 -0700

    Made module and included in class

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,30 @@
+#module Shout
+#  def self.yell_angrily(words)
+#  	words + "!!!" + " >:("
+#  end
+#  def self.yell_happily(words)
+#  	words + "!!" + " :)"
+#  end
+#end
+
 module Shout
-  def self.yell_angrily(words)
-  	words + "!!!" + " >:("
-  end
-  def self.yell_happily(words)
-  	words + "!!" + " :)"
-  end
+	def yell_angrily(words)
+		words + "!!!" + " >:("
+	end
+	def yell_happily(words)
+		words + "!!" + " :)"
+	end
 end
+
+class Coach
+	include Shout
+end
+class Child
+	include Shout
+end
+
+Bob = Coach.new
+Cindy = Child.new
+puts Bob.yell_angrily("Get the ball")
+puts Cindy.yell_happily("I caught a Pikachu")
+
