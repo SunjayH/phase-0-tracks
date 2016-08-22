ce8d9758c587c567f23a909de4e59c54bd0c3458
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-22 15:48:40 -0700

    IDK what this is but whatevs

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+//Input is a string
+//We make an empty string
+//For each character in the string, starting at the end
+//we add the character to the empty string
+
+function reverse(str){
+	var reverseStr = '';
+	for (i = str.length-1; i >= 0; i--){
+		reverseStr += str[i];
+	}
+	return reverseStr
+}
+
+console.log(reverse("Your face"));
