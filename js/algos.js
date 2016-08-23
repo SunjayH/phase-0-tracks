ef3870c9dec65abde9f2615c8391b7496c8a7f9f
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-23 15:30:41 -0700

    Made check objects for matching entries algorithm

--- js/algos.js
+++ js/algos.js
@@ -17,3 +17,27 @@ function longestWord(wordArray){
 }
 
 console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
+console.log(longestWord(["shorter string","short","super long string"]));
+console.log(longestWord(["oontz oontz oontz","dowop dowop","bing"]));
+
+//Take as input two objects
+// Set the default match boolean to false
+//For each pair in the first object, check each of the pairs in the second
+// If there's a match, toggle the match boolean to true
+// If that doesn't happen, the match boolean will stay false
+// Which is what we want
+
+function matchObjects(obj1,obj2){
+	var matchBool = false;
+	for (key1 in obj1){
+		for (key2 in obj2){
+			if (obj1[key1] == obj2[key2]){
+				matchBool = true;
+			}
+		}
+	}
+	return matchBool;
+}
+
+console.log(matchObjects({name: "Steven", age: 52},{name: "Tamir", age: 54}));
+console.log(matchObjects({name: "Steven", age: 54},{name: "Tamir", age: 54}));
