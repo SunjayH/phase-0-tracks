0c7d7a5a2f255c3a213689307168a102cbd199bb
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-23 11:39:46 -0700

    Made car function with start function

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,4 +10,16 @@ for (var i = horses.length - 1; i >= 0; i--) {
 	colorHorses[horses[i]] = colors[i];
 }
 
-console.log(colorHorses);
+//console.log(colorHorses);
+
+function Car(brand,model,color){
+	this.brand = brand;
+	this.model = model;
+	this.color = color;
+
+	this.start = function() {console.log("The engine is running!");};	
+}
+
+var newCar = new Car("Toyota", "Prius", "Cobalt");
+newCar.start();
+console.log(newCar);
