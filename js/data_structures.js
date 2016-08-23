9e7dac85d33175d9622497d4788578ef518aa116
SunjayH <lorenzianheteroceran@gmail.com>
2016-08-23 11:25:02 -0700

    Paired colors with horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,13 @@
 var colors = ["Watermelon", "Sky blue", "Forest green", "Hot pink"];
 var horses = ["Ed", "Phil", "Yasmina", "Rachel"];
+
+colors.push("Orange");
+horses.push("Emmett");
+
+var coloredHorses = {};
+
+for (var i = horses.length - 1; i >= 0; i--) {
+	coloredHorses[horses[i]] = colors[i];
+}
+
+console.log(coloredHorses);
