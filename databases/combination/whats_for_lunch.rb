837eb5344fc62dac65e47ee1b3bfc0f119029edc
SunjayH <lorenzianheteroceran@gmail.com>
2016-09-02 23:53:57 -0700

    Made method that puts new types of food in the database

--- databases/combination/whats_for_lunch.rb
+++ databases/combination/whats_for_lunch.rb
@@ -32,10 +32,7 @@ create_food_list = <<-SQL
 	SQL
 
 staples = <<-SQL
-	INSERT INTO food_type (food_name, standard_measure, is_vegetable, est_cal)
-	VALUES ("egg", "large egg(s)", "false", 78),
-	("multigrain bread", "slice(s)", "false", 109),
-	("soy milk", "cup(s)", "false", 131);
+	INSERT INTO food_type (food_name, standard_measure, is_vegetable, est_cal) VALUES ("egg", "large egg(s)", "false", 78),	("multigrain bread", "slice(s)", "false", 109),	("soy milk", "cup(s)", "false", 131);
 SQL
 
 $fridge.execute(create_fridge)
@@ -47,7 +44,87 @@ end
 
 
 #Will have four different methods:
-#Shop will allow items to be put in fridge database
 #New item will allow new types of food to be put in food type database
+#Shop will allow items to be put in fridge database
 #Suggest meal will give list of things in fridge and tell whether to go shopping
 #Eat will take items out of fridge database 
+
+#new_item will ask for each of the pieces of information about the item
+#new_item will push that information into the food_type table
+#new_item will return the id of the added item
+
+def new_item()
+	yes_values = ["Yes", "yes", "Y", "y"]
+	puts "What is the new item?"
+	food_name = gets.chomp
+	puts "Is this item a vegetable?"
+	ans = gets.chomp
+	if yes_values.any? {|yes| ans.include? yes}
+		is_vegetable = true
+	else
+		is_vegetable = false
+	end
+	puts "Google calories in #{food_name}."
+	puts "What is the unit of measure for this item (eg. slice, cup)?"
+	standard_measure = gets.chomp
+	puts "How many calories is one #{standard_measure} of this item?"
+	est_cal = gets.chomp.to_i
+
+	#Put in database
+	#if $fridge.execute("SELECT * FROM food_type WHERE food_name=#{food_name}").empty?
+	#It looks like sql doesn't like my query, oops, so we'll just let there be duplicates
+	insert_command = <<-SQL
+		INSERT INTO food_type (food_name, standard_measure, is_vegetable, est_cal) VALUES ("#{food_name}", "#{standard_measure}", "#{is_vegetable}", #{est_cal})
+	SQL
+		$fridge.execute(insert_command)
+	#else
+	#	puts "It looks like this food item is already in the database."
+	#	puts $fridge.execute("SELECT * FROM food_type")
+	#end
+end
+
+new_item
+# Shop will ask if add item
+# If yes, shop will display list of known items and ask if it's on the list
+	# If yes, shop will ask which item it is
+	# If no, shop will run new item method to add item, then ask which item
+	# Shop will ask for quantity of item
+#If no, loop breaks
+#New items will be put into a database by going through NewItemHash with each
+
+# def shop
+# 	yes_values = ["Yes", "yes", "Y", "y"]
+# 	newItemHash = {}
+# 	while i = i do
+# 		puts "Did you buy another item?"
+# 		if yes_values.any? {|yes| gets.chomp.include? yes}
+# 			puts "Is the item you bought on this list?"
+# 			puts $fridge.execute("SELECT (food_name) FROM food_type")
+# 			if yes_values.any? {|yes| gets.chomp.include? yes}
+# 				puts "What number is it on the list?"
+# 				food_id = gets.chomp.to_i
+# 			else
+# 				food_id = new_item()
+# 			end
+# 			food_measure = $fridge.execute("Select (standard_measure) FROM food_type WHERE id = #{food_id}")
+# 			puts "How many #{food_measure} did you purchase?"
+# 			food_quantity = gets.chomp.to_i
+# 		else
+# 			break
+# 		end
+# 		newItemHash[food_id] = food_quantity
+# 	end
+
+# 	#Put bought items into the database
+# 	#Figures out if the item is already in the fridge
+# 	#If it is, increase the quantity
+# 	#If it's not, make a new line
+# 	newItemHash.each do |id,quantity|
+# 		if $fridge.execute("SELECT * FROM fridge WHERE food_type=#{id}").empty?
+# 			$fridge.execute("INSERT INTO fridge (food_type, quantity) VALUES (#{id}, #{quantity})")
+# 		else
+# 			quantity += $fridge.execute("SELECT (quantity) FROM fridge WHERE food_type=#{id}")
+# 			$fridge.execute("UPDATE fridge SET quantity=#{quantity} WHERE food_type=#{id}")
+# 		end
+# 	end	
+# end
