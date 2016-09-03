2944b6d4459aa579168a090fc1442938e21b467e
SunjayH <lorenzianheteroceran@gmail.com>
2016-09-02 17:14:50 -0700

    Working out kinks in database stuff

--- databases/combination/whats_for_lunch.rb
+++ databases/combination/whats_for_lunch.rb
@@ -6,9 +6,6 @@ require 'faker'
 $fridge = SQLite3::Database.new("fridge.db")
 $fridge.results_as_hash = true
 
-$food_type = SQLite3::Database.new("food_type.db")
-$food_type.results_as_hash = true
-
 #Will have a database that is the fridge
 #In the fridge are foods with calories
 #If there's not enough calories to make a meal (ca. 1000)
@@ -22,22 +19,37 @@ create_fridge = <<-SQL
 		food_type INT,
 		quantity INT,
 		FOREIGN KEY(food_type) REFERENCES food_type(id)
-	)
+	);
 	SQL
 create_food_list = <<-SQL
 	CREATE TABLE IF NOT EXISTS food_type(
 		id INTEGER PRIMARY KEY,
+		food_name VARCHAR(255),
 		standard_measure VARCHAR(50),
 		is_vegetable BOOLEAN,
 		est_cal INT
-	)
+	);
 	SQL
 
+staples = <<-SQL
+	INSERT INTO food_type (food_name, standard_measure, is_vegetable, est_cal)
+	SELECT ("egg", "large egg(s)", "false", 78),
+	("multigrain bread", "slice(s)", "false", 109),
+	("soy milk", "cup(s)", "false", 131) WHERE NOT EXISTS (SELECT * FROM food_type);
+SQL
+
 $fridge.execute(create_fridge)
-$food_type.execute(create_food_list)
+$fridge.execute(create_food_list)
+if $fridge.execute
+	$fridge.execute(staples)
+end
+
+p $fridge.execute("SELECT * FROM fridge") do |row|
+    p row
+    end
 
 #Will have four different methods:
 #Shop will allow items to be put in fridge database
 #New item will allow new types of food to be put in food type database
-#Eat will take items out of fridge database
-#
+#Suggest meal will give list of things in fridge and tell whether to go shopping
+#Eat will take items out of fridge database 
